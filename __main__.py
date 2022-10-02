d5f4931a3744ec48493991a6cc17b8a545d52873
portion-jack <flycast0080@gmail.com>
2022-10-02 14:44:06 +0900

    __main__ changed encoding to utf-8

--- __main__.py
+++ __main__.py
@@ -1,12 +1,14 @@
 import warnings
+
 warnings.filterwarnings(action='ignore')
 
 import numpy as np
 from crwals.daily_trend import *
 import csv
-today_trends=np.array(trends_retriever())
 
-f = open('crwals/result.csv', 'a', newline='')
+today_trends = np.array(trends_retriever())
+
+f = open('crwals/result.csv', 'a', encoding='utf-8', newline='')
 wr = csv.writer(f)
 for i in range(len(today_trends)):
     wr.writerow(today_trends[i])
