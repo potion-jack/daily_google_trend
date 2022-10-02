f464596eacb636a9e624cc31b5da9598b5aeeac7
portion-jack <flycast0080@gmail.com>
2022-10-02 05:51:24 +0000

    in oracle  Sun Oct  2 05:51:24 UTC 2022

--- run.sh
+++ run.sh
@@ -1,12 +1,8 @@
-(source commit_machine/bin/activate) 2>/dev/null  || :
-
-# conda activate discord_bot_test || :
+source commit_machine/bin/activate 2>/dev/null  || :
 
 python __main__.py
 python cleaner.py
 
 git add .
-
 git commit -m "in oracle  $(date)"
-
 git push origin main
--- sh_pr.sh
+++ sh_pr.sh
@@ -1,1 +1,3 @@
-source commit_machine/bin/activate 2>dev/null  || :
+#(source commit_machine/bin/activate) 2>dev/null  || :
+source commit_machine/bin/activate 2>/dev/null || :
+pip list
