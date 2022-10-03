0de91bdb9a17be07429198f96d87ec83eb42cc90
portion-jack <flycast0080@gmail.com>
2022-10-03 09:51:46 +0900

    in local Mon Oct  3 09:51:46 KST 2022

--- sh_pr.sh
+++ sh_pr.sh
@@ -1,3 +1,22 @@
-#(source commit_machine/bin/activate) 2>dev/null  || :
-source commit_machine/bin/activate 2>/dev/null || :
-pip list
+
+# if source commit_machine/bin/activate ; then
+
+if (source commit_machine/bin/activate) ; then
+    source commit_machine/bin/activate
+    echo "venv activated"
+
+    python __main__.py
+    python clearner.py
+
+    git add .
+    git commit -m "in oracle $(date)"
+    git push origin in_oracle
+else
+    echo "conda should be activated"
+    python __main__.py
+    python cleaner.py
+
+    git add .
+    git commit -m "in local $(date)"
+    git push origin main
+fi
